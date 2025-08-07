da2506846942256caaaecafb6fd1896ffe20edb6
Mounika Regalagunta <mounikaregalagunta@gmail.com>
2025-08-07 21:54:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function Switchon(){
+    document.getElementById("bulb").src="https://thumbs.dreamstime.com/b/generative-ai-shining-light-bulb-held-hand-symbolizing-innovation-bright-ideas-modern-world-business-c-generative-ai-360594814.jpg" 
+}
+function Switchoff(){
+    document.getElementById("bulb").src="https://images.squarespace-cdn.com/content/v1/56feae0ab6aa60ebb6039bf3/0b3eaace-9dc8-4a6e-b9aa-48f91ef8c621/PLTS-12109.jpg"
+}
